b63bbf1dfd7cdb8c6d418a5b3daaa4064e20e198
Nilson Morais <nilsonsmf@live.com>
2023-03-26 22:00:12 +0100

    Dividindo entre outros componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './views/home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FornecedorListComponent } from './shared/fornecedor-list/fornecedor-list.component';
+import { FornecedorItemComponent } from './shared/fornecedor-item/fornecedor-item.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, FornecedorListComponent, FornecedorItemComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],

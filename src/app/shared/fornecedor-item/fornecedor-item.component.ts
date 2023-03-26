d00a55beddd63a569aeb3fb67ba77cf64bd303ed
Nilson Morais <nilsonsmf@live.com>
2023-03-26 22:13:32 +0100

    implementando detalhes

--- src/app/shared/fornecedor-item/fornecedor-item.component.ts
+++ src/app/shared/fornecedor-item/fornecedor-item.component.ts
@@ -3,8 +3,9 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-fornecedor-item',
   templateUrl: './fornecedor-item.component.html',
-  styleUrls: ['./fornecedor-item.component.css']
+  styleUrls: ['./fornecedor-item.component.css'],
 })
 export class FornecedorItemComponent {
   @Input() fornecedor: any;
+  public isCollapsed = true;
 }
